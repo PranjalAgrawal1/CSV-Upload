b1719d3dd217a7e4f1d85bc9d4679b72d8d17313
PranjalAgrawal1 <pranjal605@gmail.com>
2022-05-26 06:55:10 +0530

    port error resolved

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const path = require('path')
 const fs = require('fs')
 const expresslayouts = require('express-ejs-layouts');
